f33f142139f97384237547dfc841b600c7ed48e8
Marco Eidinger <eidingermarco@gmail.com>
2022-11-29 16:24:23 -0800

    improve

--- README.md
+++ README.md
@@ -1,134 +1,37 @@
 # OSInfo
+
 Cross-platform Swift Package to determine OS name and version on which the app is running
 
 ```swift
 import OSVersion
 
-print(OSInfo.shared.name) 		// iOS
-print(OSInfo.shared.version)	// 16.1
+print(OSInfo.shared.name) // iOS
+print(OSInfo.shared.version) // 16.1
 ```
+
+This package provides a unified API so that you don't need to worry about the various underlying APIs like
+- UIKit's `UIDevice.current.systemName` and `.systemVersion`
+- WatchKit's `WKInterfaceDevice.current().systemName` and `.systemVersion`
+- Foundations's `ProcessInfo.processInfo.operatingSystemVersionString` and `.operatingSystemVersion`
 
 Here are examples based on the destination:
 
-| Destination                                                  | OSInfo.shared.name                          | OSInfo().version |
+| Destination                                                  | OSInfo.shared.name                          | OSInfo().name |
 | ------------------------------------------------------------ | ------------------------------------------------------------ | ------------- |
 | iPhone                                                       | iOS                                                          | iOS           |
 | iPad                                                         | iPadOS                                                       | iPadOS        |
-| Mac (Designed for iPad)<br /><br />this might be a Mac app built with Mac Catalyst  or an iOS app running on Apple silicon. | **macOS** | iPadOS        |
+| Mac (Designed for iPad)<br /><br />~ an iOS app running on Apple silicon | **macOS** | iPadOS        |
 | Mac (Mac Catalyst)                                           | **macOS**   | iPadOS        |
 | Mac                                                         | macOS                                                      | macOS         |
 | TV                                                           | tvOS                                                         | tvOS          |
 | Watch                                                        | watchOS                                                      | watchOS       |
 | Linux                                                        | ???                                                          | ???           |
+Windows                                                      | ???           | ??? | 
+
+This package also provides an option to either get the underlying macOS version or the "iOS support version" in case of a Mac Catalyst / Mac Designed for iPad application.
 
 Note:
+- `OSInfo()` is equivalent to `OSInfo(underlyingMacOS: false)`
+- `OSInfo.shared` is equivalent to `OSInfo(underlyingMacOS: true)`
 
-- `OSInfo.shared` is equivalent to `OSInfo(mac4mac: true)`
-
-- `OSInfo()` is equivalent to `OSInfo(mac4mac: false)`
-
-
-
-https://github.com/jonathanwong/teal-swift/blob/992c185423d907ed86aa3148e0939aca11b96ae1/tealium/core/devicedata/DeviceDataOSInfo.swift
-
-```swift
-#if os(OSX)
-#else
-import UIKit
-#endif
-import Foundation
-
-#if os(watchOS)
-import WatchKit
-#endif
-
-// MARK: OS Info
-public extension DeviceData {
-    class var oSBuild: String {
-        guard let build = Bundle.main.infoDictionary?["DTSDKBuild"] as? String else {
-            return DeviceDataValue.unknown
-        }
-        return build
-
-    }
-
-    class var oSVersion: String {
-        // only available on iOS and tvOS
-        #if os(iOS)
-        return UIDevice.current.systemVersion
-        #elseif os(tvOS)
-        return UIDevice.current.systemVersion
-        #elseif os(watchOS)
-        return WKInterfaceDevice.current().systemVersion
-        #elseif os(OSX)
-        return ProcessInfo.processInfo.operatingSystemVersionString
-        #else
-        return DeviceDataValue.unknown
-        #endif
-    }
-
-    class var oSName: String {
-        // only available on iOS and tvOS
-        #if os(iOS)
-        return UIDevice.current.systemName
-        #elseif os(tvOS)
-        return UIDevice.current.systemName
-        #elseif os(OSX)
-        return "macOS"
-        #else
-        return DeviceDataValue.unknown
-        #endif
-    }
-}
-```
-
-Alternative https://github.com/lidongx/Adapty/blob/01ef0c4d6f9147cf62d19b312a1107818209f9df/Classes/UserProperties.swift
-
-```swift
-    static var OS: String {
-        #if os(macOS) || targetEnvironment(macCatalyst)
-        return "macOS \(ProcessInfo().operatingSystemVersionString)"
-        #else
-        return "\(UIDevice.current.systemName) \(UIDevice.current.systemVersion)"
-        #endif
-    }
-    
-    static var platform: String {
-        #if os(macOS) || targetEnvironment(macCatalyst)
-        return "macOS"
-        #else
-        return UIDevice.current.systemName
-        #endif
-    }
-```
-
-General Info: https://nshipster.com/swift-system-version-checking/
-
-To be tested on
-- iPhone / iOS
-- iPad / iPadOS
-- MacCatalyst
-- Mac
-- Linux
-- tvOS
-- watchOS
-
-
-P.S.: PLCrashReporter handles it like this (https://github.com/microsoft/plcrashreporter/blob/b1a342da19ed9b3af61ea2efa7656c2af30aeb7c/Source/PLCrashLogWriter.m#L459)
-
-```swift
-#if TARGET_OS_IPHONE || TARGET_OS_MAC
-    /* iOS, tvOS, macOS and Mac Catalyst */
-    {
-        NSProcessInfo *processInfo = [NSProcessInfo processInfo];
-        NSOperatingSystemVersion systemVersion = processInfo.operatingSystemVersion;
-        NSString *systemVersionString = [NSString stringWithFormat:@"%ld.%ld", (long)systemVersion.majorVersion, (long)systemVersion.minorVersion];
-        if (systemVersion.patchVersion > 0) {
-            systemVersionString = [systemVersionString stringByAppendingFormat:@".%ld", (long)systemVersion.patchVersion];
-        }
-        plprotobuf_cbinary_data_nsstring_init(&writer->system_info.version, systemVersionString);
-    }
-#else
-#error Unsupported Platform
-#endif
-```
+If you are more interesting on how to compare versions then I recommend the article [Swift System Version Checking](https://nshipster.com/swift-system-version-checking/)
--- Sources/OSInfo/OSInfo.swift
+++ Sources/OSInfo/OSInfo.swift
@@ -9,92 +9,97 @@ import Foundation
 #endif
 
 public struct OSInfo {
-    public static let shared = OSInfo(mac4mac: true)
+    public static let shared = OSInfo(underlyingMacOS: true)
 
-    private var mac4mac: Bool = false
+    private var underlyingMacOS: Bool = false
 
-    public init(mac4mac: Bool = false) {
-        self.mac4mac = mac4mac
+    public init(underlyingMacOS: Bool = false) {
+        self.underlyingMacOS = underlyingMacOS
     }
 
     public var version: String {
-        if mac4mac {
-            #if os(macOS) || os(iOS)
-            if #available(iOS 13.0, macOS 10.15, *) {
-                // true when a Mac app built with Mac Catalyst or an iOS app running on Apple silicon
-                if ProcessInfo.processInfo.isMacCatalystApp {
-                    return operatingSystemVersionFromProcess()
+        if underlyingMacOS {
+            // Mac Catalyst || Mac Designed for iPad
+            #if targetEnvironment(macCatalyst) || os(iOS)
+                if #available(iOS 13.0, macOS 10.15, *) {
+                    // true when a Mac app built with Mac Catalyst or an iOS app running on Apple silicon
+                    if ProcessInfo.processInfo.isMacCatalystApp {
+                        return operationSystemVersionFromPlist() ?? operatingSystemVersionFromProcess()
+                    }
+                } else {
+                    () // might be a native macOS app so lets continue
                 }
-            } else {
-                () // ProcessInfo.processInfo.isMacCatalystApp might not be available or running app might be native macOS or on a complete different platform so lets continue
-            }
             #endif
-            #if os(macOS) || targetEnvironment(macCatalyst)
+            #if os(macOS) // Mac
                 return operatingSystemVersionFromProcess()
-            #elseif os(iOS)
-                return UIDevice.current.systemVersion
-            #elseif os(tvOS)
-                return UIDevice.current.systemVersion
-            #elseif os(watchOS)
-                return WKInterfaceDevice.current().systemVersion
-            #elseif os(Linux) || os(Windows)
-                return operatingSystemVersionFromProcess()
-            // return ProcessInfo.processInfo.operatingSystemVersionString
-            #else
-                return DeviceDataValue.unknown
+            #elseif os(iOS) // iPhone or iPad
+            return operatingSystemVersionFromProcess()
+                //return UIDevice.current.systemVersion
             #endif
         } else {
             // only available on iOS and tvOS
-            #if os(iOS)
+            #if os(iOS) // iPhone || iPad || Mac Catalyst || Mac Designed for iPad
                 return UIDevice.current.systemVersion
-            #elseif os(tvOS)
-                return UIDevice.current.systemVersion
-            #elseif os(watchOS)
-                return WKInterfaceDevice.current().systemVersion
-            #elseif os(Linux) || os(Windows)
+            #elseif os(macOS) // Mac
                 return operatingSystemVersionFromProcess()
-            // return ProcessInfo.processInfo.operatingSystemVersionString
-            #else
-                fatalError("not supported")
             #endif
         }
+
+        #if os(tvOS)
+            return UIDevice.current.systemVersion
+        #elseif os(watchOS)
+            return WKInterfaceDevice.current().systemVersion
+        #elseif os(Linux) || os(Windows)
+            return operatingSystemVersionFromProcess()
+        // return ProcessInfo.processInfo.operatingSystemVersionString
+        #else
+            fatalError("not supported")
+        #endif
     }
 
     public var name: String {
-        if mac4mac {
-            #if os(macOS) || targetEnvironment(macCatalyst)
-                return "macOS"
-            #elseif os(iOS)
-                return UIDevice.current.systemName
-            #elseif os(tvOS)
-                return UIDevice.current.systemName
-            #elseif os(watchOS)
-                return WKInterfaceDevice.current().systemName
-            #elseif os(Linux)
-                return "Linux"
-            #elseif os(Windows)
-                return "Windows"
-            #else
-                fatalError("not supported")
-            #endif
-        } else {
-            // only available on iOS and tvOS
-            #if os(iOS)
-                return UIDevice.current.systemName
-            #elseif os(tvOS)
-                return UIDevice.current.systemName
-            #elseif os(watchOS)
-                return WKInterfaceDevice.current().systemName
-            #elseif os(OSX)
-                return "macOS"
-            #elseif os(Linux)
-                return "Linux"
-            #elseif os(Windows)
-                return "Windows"
-            #else
-                fatalError("not supported")
-            #endif
-        }
+        #if os(tvOS)
+            return UIDevice.current.systemName
+        #elseif os(watchOS)
+            return WKInterfaceDevice.current().systemName
+        #elseif os(Linux)
+            return "Linux"
+        #elseif os(Windows)
+            return "Windows"
+        #elseif os(macOS) || os(iOS)
+            if underlyingMacOS {
+                if #available(iOS 13.0, macOS 10.15, *) { // Mac Catalyst || Mac Designed for iPad
+                    // true when a Mac app built with Mac Catalyst or an iOS app running on Apple silicon
+                    if ProcessInfo.processInfo.isMacCatalystApp {
+                        return "macOS"
+                    }
+                } else {
+                    () // might be native macOS so lets continue
+                }
+                #if os(macOS)
+                    return "macOS"
+                #elseif os(iOS) // Phone (= iOS) || iPad (= iPadOS)
+                    return UIDevice.current.systemName
+                #endif
+            } else {
+                #if os(macOS)
+                    return "macOS"
+                #else // Phone || iPad || Mac Catalyst
+                    return UIDevice.current.systemName
+                #endif
+            }
+        #else
+            fatalError("not supported")
+        #endif
+    }
+    
+    /// returns `ProductVersion` from `/System/Library/CoreServices/.SystemVersionPlatform.plist`
+    ///
+    /// useful as  `ProcessInfo` will return iOS support version for "Mac Designed for iPad" destination which might not be desired
+    private func operationSystemVersionFromPlist() -> String? {
+        let sv = NSDictionary(contentsOfFile: "/System/Library/CoreServices/.SystemVersionPlatform.plist")
+        let versionString = sv?.object(forKey: "ProductVersion") as? String
+        return versionString
     }
 
     private func operatingSystemVersionFromProcess() -> String {
