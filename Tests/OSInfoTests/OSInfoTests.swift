64cca4b95471acf95a570211e8c81b6b1e069f38
Marco Eidinger <eidingermarco@gmail.com>
2022-11-29 17:07:55 -0800

    improve testing

--- Tests/OSInfoTests/OSInfoTests.swift
+++ Tests/OSInfoTests/OSInfoTests.swift
@@ -3,8 +3,10 @@ import XCTest
 
 final class OSInfoTests: XCTestCase {
     func testNameAndVersion() throws {
-        print(OSInfo.shared.name)
-        print(OSInfo.shared.version)
+        print("OSInfo.shared.name: \(OSInfo.shared.name)")
+        print("OSInfo.shared.version: \(OSInfo.shared.version)")
+        print("OSInfo().name: \(OSInfo().name)")
+        print("OSInfo().version: \(OSInfo().version)")
         XCTAssertTrue(true)
     }
 }
